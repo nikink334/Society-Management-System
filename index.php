3a3f9f5058b84cdfe8dda326e5987d022b3ae8f2
nikink334 <nikink334@gmail.com>
2024-03-07 08:19:51 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> Ganga Village </title>
+
+    <link rel="stylesheet" href="css\style.css">
+
+
+</head>
+
+<body>
+    
+
+    <!-- Navbar -->
+    <nav class="navbar">
+		<div class="navdiv">
+			<div class="logo"><a href="#"> Ganga Village </a> </div>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<button><a href="#">SignIn</a></button>
+				<button><a href="#">SignUp</a></button>
+			</ul>
+		</div>
+	</nav>
+
+
+</body>
+
+</html>
